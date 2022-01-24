37523ed4fa85a99d9fb46b42c9603b26dd9d39b6
ezen <ezen@DESKTOP-O03MT84>
2022-01-24 11:56:54 +0900

    Commit

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,22 @@
+use world;
+
+show tables;
+
+-- 주석
+-- 테이블 생성
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+-- 테이블 삭제
+
